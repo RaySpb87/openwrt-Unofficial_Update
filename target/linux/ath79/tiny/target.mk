9de75abeb6c7a2fdc710c37bd63478a3f48f8229
vboxuser <vboxuser@Mint20-3.myguest.virtualbox.org>
2026-08-19 21:40:33 +0300

    ath79/tiny: disable opkg and use 512K squashfs block size by default

--- target/linux/ath79/tiny/target.mk
+++ target/linux/ath79/tiny/target.mk
@@ -2,6 +2,7 @@ BOARDNAME:=Devices with small flash
 FEATURES += squashfs small_flash
 
 DEFAULT_PACKAGES += wpad-mini
+DEFAULT_PACKAGES := $(filter-out opkg,$(DEFAULT_PACKAGES))
 
 define Target/Description
 	Build firmware images for Atheros AR71xx/AR913x/AR934x based boards with small flash
