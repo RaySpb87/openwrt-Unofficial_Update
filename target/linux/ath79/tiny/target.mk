0ff285fdf5a3c3b94b1188bba7e5165e2b0e1ce9
vboxuser <vboxuser@Mint20-3.myguest.virtualbox.org>
2026-08-21 21:55:30 +0300

    ath79/tiny: disable ppp and ppp-mod-pppoe by default

--- target/linux/ath79/tiny/target.mk
+++ target/linux/ath79/tiny/target.mk
@@ -3,6 +3,7 @@ FEATURES += squashfs small_flash
 
 DEFAULT_PACKAGES += wpad-mini
 DEFAULT_PACKAGES := $(filter-out opkg,$(DEFAULT_PACKAGES))
+DEFAULT_PACKAGES.router := $(filter-out ppp ppp-mod-pppoe,$(DEFAULT_PACKAGES.router))
 
 define Target/Description
 	Build firmware images for Atheros AR71xx/AR913x/AR934x based boards with small flash
